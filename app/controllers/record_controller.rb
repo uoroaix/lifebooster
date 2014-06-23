b6ea0953b9d9634446568a74639db96437e425c4
Alex Kuang <uoroaix@gmail.com>
2014-06-23 13:49:53 -0700

    more gracefull fallbacks for controller

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -13,20 +13,31 @@ class RecordController < ApplicationController
     end
     connector = Hexoskin::Connector.new(username, ENV['HEXOSKIN_PASSWORD'])
 
-    data_hr = (JSON.parse(connector.get("/api/v1/data/?datatype=19&record=#{@record_id}", {}).body))[0]["data"]["19"]
-    data_hr.each do |array|
-      @plots_hr.push [(array[0]/256*1000), array[1]]
+    data_hr = connector.get("/api/v1/data/?datatype=19&record=#{@record_id}", {}).body
+    if data_hr != "[]"
+    data_hr = (JSON.parse(data_hr))[0]["data"]["19"]
+      data_hr.each do |array|
+        @plots_hr.push [(array[0]/256*1000), array[1]]
+      end
     end
 
-    data_br = (JSON.parse(connector.get("/api/v1/data/?datatype=33&record=#{@record_id}", {}).body))[0]["data"]["33"]
-    data_br.each do |array|
-      @plots_br.push [(array[0]/256*1000), array[1]]
+    data_br = connector.get("/api/v1/data/?datatype=33&record=#{@record_id}", {}).body
+    if data_br != "[]"
+      data_br = (JSON.parse(data_br))[0]["data"]["33"]
+      data_br.each do |array|
+        @plots_br.push [(array[0]/256*1000), array[1]]
+      end
     end
 
-    data_ca = (JSON.parse(connector.get("/api/v1/data/?datatype=53&record=#{@record_id}", {}).body))[0]["data"]["53"]
-    data_ca.each do |array|
-      @plots_ca.push [(array[0]/256*1000), array[1]]
+    data_ca = connector.get("/api/v1/data/?datatype=53&record=#{@record_id}", {}).body
+    if data_br != "[]"
+      data_ca = (JSON.parse(data_ca))[0]["data"]["53"]
+      data_ca.each do |array|
+        @plots_ca.push [(array[0]/256*1000), array[1]]
+      end
     end
+
+    flash.now[:alert] = "There are not data for this record" if data_hr == "[]" && data_br == "[]" && data_ca == "[]"
 
   end
 
